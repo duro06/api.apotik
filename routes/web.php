2e59a8f1d06539da0ef22b985763af837b25055c
duro06 <andi.meka.025@gmail.com>
2025-06-16 21:40:07 +0700

    test migration and seed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/autogen', function () {
+    $user = User::limit(10)->get();
+    return $user;
 });
