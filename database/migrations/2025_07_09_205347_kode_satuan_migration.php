202fa36e1583fcb71c1427063614ba17392858da
duro06 <andi.meka.025@gmail.com>
2025-07-10 03:59:59 +0700

    migration kode

---
+++ database/migrations/2025_07_09_205347_kode_satuan_migration.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        DB::unprepared("DROP PROCEDURE IF EXISTS kode_satuan;");
+
+        DB::unprepared("
+
+                CREATE DEFINER=`admin`@`%` PROCEDURE `kode_satuan`(OUT nomor INT(12))
+                BEGIN
+
+                    DECLARE jml INT DEFAULT 0;
+
+                    DECLARE cur_query CURSOR FOR select kode_satuan from counter;
+
+                    DECLARE CONTINUE HANDLER FOR NOT FOUND SET jml = 1;
+
+                    OPEN cur_query;
+
+                    WHILE (NOT jml) DO
+
+                        FETCH cur_query INTO nomor;
+
+                        IF NOT jml THEN
+
+                            update counter set kode_satuan=kode_satuan+1;
+
+                        END IF;
+
+                    END WHILE;
+
+
+
+                    CLOSE cur_query;
+
+
+
+                END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        DB::unprepared("DROP PROCEDURE IF EXISTS kode_satuan;");
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        // User::factory(5)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('counter')->insert([
+            'kode_barang' => 0,
+            'kode_pelanggan' => 0,
+            'kode_satuan' => 0,
+            'kode_supplier' => 0,
+            'kode_jabatan' => 0
+        ]);
     }
 }
