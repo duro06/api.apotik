ec7c7e247d20c6e55d0d012650b9a24aa59ca36c
kentangkotak <ariezlnd69@gmail.com>
2025-08-03 19:27:05 +0700

    fix : dokter

---
+++ app/Http/Controllers/Api/Master/DokterController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api\Master;
+
+use App\Helpers\Formating\FormatingHelper;
+use App\Helpers\ResponseHelper;
+use App\Http\Controllers\Controller;
+use App\Models\Master\Dokter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DokterController extends Controller
+{
+    public function index()
+    {
+        $req = [
+            'order_by' => request('order_by') ?? 'created_at',
+            'sort' => request('sort') ?? 'asc',
+            'page' => request('page') ?? 1,
+            'per_page' => request('per_page') ?? 10,
+        ];
+
+        $raw = Dokter::query();
+
+        $raw->when(request('q'), function ($q) {
+            $q->where(function ($query) {
+                $query->where('nama_dokter', 'like', '%' . request('q') . '%')
+                    ->orWhere('kode', 'like', '%' . request('q') . '%');
+            });
+        })
+        ->orderBy($req['order_by'], $req['sort']);
+        $totalCount = (clone $raw)->count();
+        $data = $raw->simplePaginate($req['per_page']);
+
+        $resp = ResponseHelper::responseGetSimplePaginate($data, $req, $totalCount);
+        return new JsonResponse($resp);
+    }
+
+    public function store(Request $request)
+    {
+        $kode = $request->kode;
+        $validated = $request->validate([
+            'nama_dokter' => 'required',
+            'kode' => 'nullable',
+             'alamat' => 'required',
+        ], [
+            'nama.required' => 'Nama wajib diisi.'
+        ]);
+
+        if (!$kode) {
+            DB::select('call kode_dokter(@nomor)');
+            $nomor = DB::table('counter')->select('kode_dokter')->first();
+            $validated['kode'] = FormatingHelper::genKodeDinLength($nomor->kode_jabatan, 4, 'DK');
+        }
+
+        $data = Dokter::updateOrCreate(
+            [
+                'kode' => $validated['kode']
+            ],
+            $validated
+        );
+        return new JsonResponse([
+            'data' => $data,
+            'message' => 'Data Jabatan berhasil disimpan'
+        ]);
+    }
+
+}
---
+++ database/migrations/2025_08_03_122246_create_procedure_kode_dokter.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared("DROP PROCEDURE IF EXISTS kode_dokter;");
+
+        DB::unprepared("
+
+                CREATE DEFINER=`admin`@`%` PROCEDURE `kode_dokter`(OUT nomor INT(12))
+                BEGIN
+
+                    DECLARE jml INT DEFAULT 0;
+
+                    DECLARE cur_query CURSOR FOR select kode_dokter from counter;
+
+                    DECLARE CONTINUE HANDLER FOR NOT FOUND SET jml = 1;
+
+                    OPEN cur_query;
+
+                    WHILE (NOT jml) DO
+
+                        FETCH cur_query INTO nomor;
+
+                        IF NOT jml THEN
+
+                            update counter set kode_dokter=kode_dokter+1;
+
+                        END IF;
+
+                    END WHILE;
+
+
+
+                    CLOSE cur_query;
+
+
+
+                END
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('procedure_kode_dokter');
+    }
+};
