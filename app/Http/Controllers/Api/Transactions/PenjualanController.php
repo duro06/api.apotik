218f2b7906c23eaa70c0c5d58e7e603fc2825a90
duro06 <andi.meka.025@gmail.com>
2025-09-13 15:22:59 +0700

    rev : hpp validasi dan simpan

--- app/Http/Controllers/Api/Transactions/PenjualanController.php
+++ app/Http/Controllers/Api/Transactions/PenjualanController.php
@@ -138,6 +138,7 @@ class PenjualanController extends Controller
             'harga_jual' => 'required', // ini dari master
             'harga_beli' => 'required', // ini dari master
             // 'hpp' => 'required', // ini di taruh di master, hasil query dari 5 harga terakhir
+            'hpp' => 'nullable', // ini di taruh di master, hasil query dari 5 harga terakhir
             'id_penerimaan_rinci' => 'required', // ini dari stok
             'nopenerimaan' => 'required', // ini dari stok
             'nobatch' => 'required', // ini dari stok
@@ -154,7 +155,7 @@ class PenjualanController extends Controller
             'nobatch.required' => 'Nomor Batch belum di ikutkan, silahkan kontak penyedia IT',
             'tgl_exprd.required' => 'Tanggal Expired Obat di ikutkan, silahkan kontak penyedia IT',
             'id_stok.required' => 'id Stok belum di ikutkan, silahkan kontak penyedia IT',
-            'hpp.required' => 'HPP belum di ikutkan, silahkan kontak penyedia IT',
+            // 'hpp.required' => 'HPP belum di ikutkan, silahkan kontak penyedia IT',
         ]);
         try {
             DB::beginTransaction();
@@ -196,7 +197,7 @@ class PenjualanController extends Controller
                 'tgl_exprd' => $validated['tgl_exprd'],
                 'harga_jual' => $validated['harga_jual'],
                 'harga_beli' => $validated['harga_beli'],
-                'hpp' => $validated['hpp'],
+                'hpp' => $validated['hpp'] ?? $validated['harga_beli'],
                 'subtotal' => $subtotal,
                 'kode_user' => $user->kode,
             ]);
